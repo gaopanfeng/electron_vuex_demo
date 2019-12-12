e2e68935a77b4f478b920870a095108bac894d5d
gaopanfeng <gaopanfeng@sogou-inc.com>
2019-12-12 11:51:41 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    test
+    <h1>Vuex Electron</h1>
+    <h3>Example of usage</h3>
+
+    <div class="count">
+      {{count}}
+    </div>
+
+    <div class="buttons">
+      <button v-on:click="increment">Increment</button>
+      <button v-on:click="decrement">Decrement</button>
+    </div>
+
+    <p class="note">
+      <small>Number automatically increments each 5 seconds<br>from main process</small>
+    </p>
+  </div>
+</template>
+
+
+<script>
+  import { mapState, mapActions } from 'vuex'
+
+  // In renderer process to call actions you need to use `dispatch` or `mapActions`
+  // Don't use `commit` because actions fired via `commit` will not be shared between processes
+
+  export default {
+    computed: mapState(['count']),
+    methods: mapActions(['increment', 'decrement'])
+  }
+</script>
+
+<style type="scss">
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import { createPersistedState, createSharedMutations } from 'vuex-electron'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+
+  actions: {
+    increment (store) {
+      store.commit('increment')
+    },
+    decrement (store) {
+      store.commit('decrement')
+    },
+    check (store) {
+
+    }
+  },
+
+  mutations: {
+    increment (state) {
+      state.count++
+    },
+    decrement (state) {
+      state.count--
+    }
+  },
+
+  plugins: [createPersistedState(), createSharedMutations()],
+  strict: process.env.NODE_ENV !== 'production'
+})
